3b1d1e943bcb1059ef4d282c47bd981c46f0d147
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-02-11 17:39:00 +0100

    initial commit

---
+++ src/main/java/ma/usmba/mscompteservice/MsCompteServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.usmba.mscompteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCompteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/usmba/mscompteservice/MsCompteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.usmba.mscompteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCompteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
