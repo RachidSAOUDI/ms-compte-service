d7872e863ec65516ec4ad2c1cbbbd64fc39f4ab5
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-02-12 00:25:51 +0100

    find methd

--- src/main/java/ma/usmba/mscompteservice/repositories/CompteRepository.java
+++ src/main/java/ma/usmba/mscompteservice/repositories/CompteRepository.java
@@ -1,9 +1,16 @@
 package ma.usmba.mscompteservice.repositories;
 
 import ma.usmba.mscompteservice.entities.Compte;
+import ma.usmba.mscompteservice.enums.TypeCompte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface CompteRepository extends JpaRepository<Compte, Long> {
+    @RestResource(path = "/byType")
+    List<Compte> findByType(@Param(value = "type") TypeCompte typeCompte);
 }
